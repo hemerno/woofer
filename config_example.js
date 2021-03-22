9379da9a440ccac1b638cedf5b7149db9a44ce64
Nikita <lozgachev@posteo.net>
2021-03-22 23:01:31 +0300

    add initialisation files and config example

---
+++ config_example.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * config.js
+ *
+ * MONGODB - данные для работы с серверами MongoDB
+ * SECRET_KEY - ключ для генерации токена при регистрации и логине пользователя
+ *
+ */
+module.exports = {
+  MONGODB: 'mongodb+srv://<MONGODB SERVER ROUTE>',
+  SECRET_KEY: '<SECRET KEY>',
+};
